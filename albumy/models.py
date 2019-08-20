7936f634b6665b16e786fffad6cbdc13720cb0f9
yangtao <yangtao@example.com>
2019-08-20 17:25:15 +0800

    role and access model

--- albumy/models.py
+++ albumy/models.py
@@ -4,6 +4,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from .extensions import db
 
+
+# relationship table
+roles_permissions = db.Table('roles_permissions',
+                             db.Column('role_id', db.Integer, db.ForeignKey('role.id')),
+                             db.Column('permission_id', db.Integer, db.ForeignKey('permission.id'))
+                            )
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, index=True)
@@ -15,6 +23,9 @@ class User(db.Model, UserMixin):
     location = db.Column(db.String(50))
     member_since = db.Column(db.DateTime, default=datetime.utcnow)
     confirmed = db.Column(db.Boolean, default=False)
+    
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    role = db.relationship('Role', back_populates='users')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -22,3 +33,38 @@ class User(db.Model, UserMixin):
     def validate_password(self, password):
         return check_password_hash(self.password_hash, password)
     
+
+class Permission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    roles = db.relationship('Role', secondary=roles_permissions, back_populates='permissions')
+
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name =db.Column(db.String(30), unique=True)
+    users = db.relationship('User', back_populates='role')
+    permissions = db.relationship('Permission', back_populates='roles')
+
+    @staticmethod
+    def init_role():
+        roles_permission_map = {
+            'Locked': ['FOLLOW', 'COLLECT'],
+            'User': ['FOLLOW', 'COLLECT', 'COMMENT', 'UPLOAD'],
+            'Moderator': ['FOLLOW', 'COLLECT', 'COMMENT', 'UPLOAD', 'MODERATRE'],
+            'Administrator': ['FOLLOW', 'COLLECT', 'COMMENT', 'UPLOAD', 'MODERATRE', 'ADMINISTER']
+        }
+
+        for role_name in roles_permission_map:
+            role = Role.query.filter_by(name=role_name).first()
+            if role is None:
+                role = Role(name=role_name)
+                db.session.add(role)
+            role.permissions = []
+            for permission_name in roles_permissions_map[role_name]:
+                permission = Permission.query.filter_by(name=permission_name).first()
+                if permission is None:
+                    permission = Permission(name=permission_name)
+                    db.session.add(permission)
+                role.permissions.append(permission)
+        db.session.commit()
