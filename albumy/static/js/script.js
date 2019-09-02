08333e0a6f2ffd5503f87a90e4a86a615897c600
yangtao <yangtao_cqu@163.com>
2019-09-02 12:44:46 +0800

    change ajax.py and script.js

--- albumy/static/js/script.js
+++ albumy/static/js/script.js
@@ -1,9 +1,64 @@
 $(function () {
     var flash = null;
 
+    function toast(body) {
+        clearTimeout(flash);
+        var $toast = $('#toast');
+        $toast.text(body).fadeIn();
+        flash = setTimeout(function () {
+            $toast.fadeOut();
+        }, 3000);
+    }
+
+    var hover_timer = null;
+
+    function show_profile_popover(e) {
+        var $el = $(e.target);
+
+        hover_timer = setTimeout(function () {
+            hover_timer = null;
+            $.ajax({
+                type: 'GET',
+                url: $el.data('href'),
+                success: function (data) {
+                    $el.popover({
+                        html: true,
+                        content: data,
+                        trigger: 'manual',
+                        animation: false
+                    });
+                    $el.popover('show');
+                    $('.popover').on('mouseleave', function () {
+                        setTimeout(function () {
+                            $el.popover('hide');
+                        }, 200);
+                    });
+                },
+                error: function () {
+                    toast('Server error, please try again later.');
+                }
+            });
+        }, 500);
+    }
+
+    function hide_profile_popover(e) {
+        var $el = $(e.target);
+
+        if (hover_timer) {
+            clearTimeout(hover_timer);
+            hover_timer = null;
+        } else {
+            setTimeout(function () {
+                if (!$('.popover:hover').length) {
+                    $el.popover('hide');
+                }
+            }, 200);
+        }
+    }
+
     $('#description-btn').click(function () {
         $('#description').hide();
-        $('#description-form').show();    
+        $('#description-form').show();
     });
     $('#cancel-description').click(function() {
         $('#description-form').hide()
